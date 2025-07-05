0804ca51050fc077e95ca50a1d000a16aaf3f7e5
codeecology <codeecology1@gmail.com>
2025-07-05 04:40:11 +0330

    Create preload.js

---
+++ v2ray-config-tester/src/preload.js
@@ -0,0 +1,29 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+// یک API امن برای ارتباط بین پردازه رندر و پردازه اصلی ایجاد می‌کنیم
+contextBridge.exposeInMainWorld('api', {
+    // Data Management
+    getAllData: () => ipcRenderer.invoke('data:get-all'),
+    saveAllData: (data) => ipcRenderer.send('data:save-all', data),
+    clearAllData: () => ipcRenderer.invoke('data:clear-all'),
+
+    // File System & Dialogs
+    importTextFile: () => ipcRenderer.invoke('file:import-text'),
+    exportFile: (options) => ipcRenderer.invoke('file:export', options),
+
+    // Network & QR
+    fetchSubscription: (url) => ipcRenderer.invoke('network:fetch-sub', url),
+    generateQRCode: (link) => ipcRenderer.invoke('qr:generate', link),
+
+    // Testing
+    startTests: (data) => ipcRenderer.send('test:start', data),
+    stopTests: () => ipcRenderer.send('test:stop'),
+    onTestResult: (callback) => ipcRenderer.on('test:result', (_event, ...args) => callback(...args)),
+    onTestProgress: (callback) => ipcRenderer.on('test:progress', (_event, ...args) => callback(...args)),
+    onTestFinish: (callback) => ipcRenderer.on('test:finish', (_event, ...args) => callback(...args)),
+
+    // Proxy Connection
+    connectProxy: (link) => ipcRenderer.send('proxy:connect', link),
+    disconnectProxy: () => ipcRenderer.send('proxy:disconnect'),
+    onProxyStatusChange: (callback) => ipcRenderer.on('proxy:status', (_event, ...args) => callback(...args)),
+});
